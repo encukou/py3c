176f04d605930967b085e96e1e674507b237626f
Petr Viktorin <pviktori@redhat.com>
2016-01-23 20:31:30 +0100

    Include tpflags.h in tests

--- test/test_py3c.c
+++ test/test_py3c.c
@@ -3,6 +3,7 @@
 #include <py3c.h>
 #include <py3c/capsulethunk.h>
 #include <py3c/fileshim.h>
+#include <py3c/tpflags.h>
 
 #define UTF8_STRING "test string \xe1\xba\x87\xc3\xad\xc5\xa5\xc4\xa7 \xc5\xae\xc5\xa2\xe1\xb8\x9e\xe2\x88\x9e \xe2\x98\xba"
 #define FORMAT_STRING "<%s:%d>"

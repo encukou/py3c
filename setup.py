22c45d5e003e058f5c0b2359853fabae4b045a41
Petr Viktorin <encukou@gmail.com>
2021-01-07 16:11:42 +0100

    Move static package information to setup.cfg

--- setup.py
+++ setup.py
@@ -11,16 +11,5 @@ def find_headers():
             if filename.endswith('.h'):
                 yield os.path.join(root, filename)
 
-with open('README.rst') as f:
-    long_description = f.read()
-
-setup(
-    name='py3c',
-    version='1.3',
-    description='Python compatibility headers',
-    long_description=long_description,
-    author='Petr Viktorin',
-    author_email='encukou@gmail.com',
-    url='http://py3c.readthedocs.io/',
-    headers=list(find_headers()),
-    platforms='any')
+# Static information is in setup.cfg.
+setup(headers=list(find_headers()))
